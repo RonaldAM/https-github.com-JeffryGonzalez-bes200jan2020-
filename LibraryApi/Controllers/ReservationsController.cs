ac117b3a13708d0585e1514e4728ac067df0be38
Jeffry Gonzalez <jeff@hypertheory.com>
2020-01-28 20:15:21 +0000

    Done with Tuesday

--- LibraryApi/Controllers/ReservationsController.cs
+++ LibraryApi/Controllers/ReservationsController.cs
@@ -2,6 +2,7 @@
 using LibraryApi.Models;
 using LibraryApi.Services;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -13,10 +14,12 @@ namespace LibraryApi.Controllers
     {
 
         LibraryDataContext Context;
+        ISendMessagesToTheReservationProcessor reservationProcessor;
 
-        public ReservationsController(LibraryDataContext context)
+        public ReservationsController(LibraryDataContext context, ISendMessagesToTheReservationProcessor reservationProcessor)
         {
             Context = context;
+            this.reservationProcessor = reservationProcessor;
         }
 
         [HttpPost("/reservations")]
@@ -37,13 +40,75 @@ namespace LibraryApi.Controllers
             // 3. That that reservation and map it to a reservation response.
             // 4. return a 201 with a location and attach a GetReservationItemResponse
             var response = MapIt(reservation);
+
+            reservationProcessor.SendForProcessing(response);
+
             return CreatedAtRoute("reservations#getbyid", new { id = response.Id }, response);
         }
 
         [HttpGet("/reservations/{id:int}", Name ="reservations#getbyid")]
         public async Task<ActionResult<GetReservationItemResponse>> GetById(int id)
         {
-            return Ok(); // come back to this.
+            var reservation = await Context.Reservations
+                .Where(r => r.Id == id)
+                .SingleOrDefaultAsync();
+
+            return this.Maybe(MapIt(reservation));
+            
+        }
+
+
+        [HttpPost("/reservations/approved")]
+        [ValidateModel]
+        public async Task<ActionResult> ApproveReservation([FromBody] GetReservationItemResponse reservation)
+        {
+            var storedReservation = await Context.Reservations.SingleOrDefaultAsync(r => r.Id == reservation.Id);
+            if(storedReservation == null)
+            {
+                return BadRequest();
+            }
+            storedReservation.Status = ReservationStatus.Approved;
+            await Context.SaveChangesAsync();
+            return Accepted(); // could also return a 201, but the location header isn't really changing.
+
+        }
+
+        [HttpGet("/reservations/approved")]
+        public async Task<ActionResult<Collection<GetReservationItemResponse>>> GetAllApprovedReservations()
+        {
+            var reservations = await Context.Reservations.Where(r => r.Status == ReservationStatus.Approved).ToListAsync();
+
+            var response = new Collection<GetReservationItemResponse>
+            {
+                Data = reservations.Select(r => MapIt(r)).ToList()
+            };
+            return Ok(response);
+        }
+
+
+        [HttpGet("/reservations/pending")]
+        public async Task<ActionResult<Collection<GetReservationItemResponse>>> GetAllPendingReservations()
+        {
+            var reservations = await Context.Reservations.Where(r=> r.Status == ReservationStatus.Pending).ToListAsync();
+
+            var response = new Collection<GetReservationItemResponse>
+            {
+                Data = reservations.Select(r => MapIt(r)).ToList()
+            };
+            return Ok(response);
+        }
+
+
+        [HttpGet("/reservations")]
+        public async Task<ActionResult<Collection<GetReservationItemResponse>>> GetAllReservations()
+        {
+            var reservations = await Context.Reservations.ToListAsync();
+
+            var response = new Collection<GetReservationItemResponse>
+            {
+                Data = reservations.Select(r => MapIt(r)).ToList()
+            };
+            return Ok(response);
         }
 
         private GetReservationItemResponse MapIt(Reservation reservation)
